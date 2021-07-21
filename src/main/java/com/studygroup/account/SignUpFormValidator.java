24e606d63b51f80004650a6338525a611cf2c80c
94101253 <jejeong@biztechpartners.co.kr>
2021-07-21 14:26:02 +0900

    회원가입 폼 서브밋 처리

---
+++ src/main/java/com/studygroup/account/SignUpFormValidator.java
@@ -0,0 +1,29 @@
+package com.studygroup.account;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+public class SignUpFormValidator implements Validator {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public boolean supports(Class<?> aClass){
+        return aClass.isAssignableFrom(SignUpForm.class);
+    }
+
+    @Override
+    public void validate(Object object , Errors errors){
+        SignUpForm signupForm = (SignUpForm)object;
+        if(accountRepository.existsByEmail(signupForm.getEmail())){
+            errors.rejectValue("email","invalid.email",new Object[]{signupForm.getEmail()},"이미 사용중인 이메일입니다.");
+        }
+        if(accountRepository.existsByNickname(signupForm.getNickname())){
+            errors.rejectValue("nickname","invalid.nickname",new Object[]{signupForm.getEmail()},"이미 사용중인 닉네임입니다.");
+        }
+    }
+}
